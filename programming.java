5a7882f699e2e7d9a1995581171ea9a2f83ce017
Sibt Ul Hassan <110232196+hassan-ali121@users.noreply.github.com>
2023-10-27 13:34:24 +0100

    just added the first file

---
+++ programming.java
@@ -0,0 +1,14 @@
+public class programming
+{
+public static void main(String[] args)
+{
+    int mark=10;
+    if(mark < 0)
+    {
+    System.out.println("Mark must be greater than 0");
+    }
+    else{
+        System.out.println("mark is smaller than 10");
+    }
+}
+}
