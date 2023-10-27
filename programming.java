16634fef25ef4c9a5235bd1c68bf621f6c5c89ec
Sibt Ul Hassan <110232196+hassan-ali121@users.noreply.github.com>
2023-10-27 18:36:56 +0100

    added first

--- programming.java
+++ programming.java
@@ -2,13 +2,14 @@ public class programming
 {
 public static void main(String[] args)
 {
-    int mark=10;
-    if(mark < 0)
+    int mark=0;
+    if(mark < 10)
     {
     System.out.println("Mark must be greater than 0");
     }
     else{
         System.out.println("mark is smaller than 10");
+
     }
 }
 }
